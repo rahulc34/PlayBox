23e642d41746d2be2186bb80fd2da2eac1a1106b
Rahul-git376 <chaurasiyarahul500@gmail.com>
2025-03-30 12:01:37 +0530

    utitls code setup for error handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
+import dotenv from "dotenv";
 
-connectDB();
+dotenv.config();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+  });
