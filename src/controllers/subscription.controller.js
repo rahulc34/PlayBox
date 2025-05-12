826890d41063871cae9973d17c37bc7abb7c1174
Rahul-git376 <chaurasiyarahul500@gmail.com>
2025-05-12 10:34:09 +0530

    complete the subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,7 +7,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  const userId = req.user?._id;
+  const userId = req.user?._id.toString();
   // TODO: toggle subscription
 
   if (!isValidObjectId(channelId)) {
@@ -23,13 +23,12 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     subscriber: userId,
   };
 
-  const isSubscribed = await Subscription.find(query);
+  const isSubscribed = await Subscription.findOne(query);
 
-  if (!isSubscribed) {
-    await Subscription.create(query);
-  } else {
-    await Subscription.deleteOne(query);
-  }
+  console.log(isSubscribed);
+  !isSubscribed
+    ? await Subscription.create(query)
+    : await Subscription.deleteOne(query);
 
   // Get updated subscription count
   const subscriptionCount = await Subscription.countDocuments({
@@ -50,17 +49,144 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-
+  const userId = req.user._id.toString();
   if (!isValidObjectId(channelId)) {
     throw new ApiError(400, "Invalid channel ID");
   }
 
-  
+  if (channelId !== userId) {
+    throw new ApiError(400, "you are not authorized");
+  }
+
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: { channel: new mongoose.Types.ObjectId(channelId) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $unwind: "$user",
+    },
+    {
+      $addFields: {
+        _id: "$user._id",
+        username: "$user.username",
+        fullname: "$user.fullname",
+        avatar: "$user.avatar",
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        username: 1,
+        fullname: 1,
+        avatar: 1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscribers,
+        "subscribers of channel is fetched successfully"
+      )
+    );
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+  const userId = req.user._id.toString();
+
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "invalid id");
+  }
+
+  if (subscriberId !== userId) {
+    throw new ApiError(404, "unauthorize access");
+  }
+
+  const subscribedChannel = await Subscription.aggregate([
+    {
+      $match: { subscriber: new mongoose.Types.ObjectId(subscriberId) },
+    },
+    {
+      $lookup: {
+        as: "channel",
+        localField: "channel",
+        foreignField: "_id",
+        from: "users",
+        pipeline: [
+          {
+            $lookup: {
+              as: "subscribers",
+              localField: "_id",
+              foreignField: "channel",
+              from: "subscriptions",
+            },
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: "$subscribers",
+              },
+              isSubscibed: {
+                $cond: {
+                  if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$channel",
+    },
+    {
+      $addFields: {
+        _id: "$channel._id",
+        username: "$channel.username",
+        email: "$channel.email",
+        fullname: "$channel.fullname",
+        avatar: "$channel.avatar",
+        subscribersCount: "$channel.subscribersCount",
+        isSubscibed: "$channel.isSubscibed",
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        username: 1,
+        fullname: 1,
+        email: 1,
+        avatar: 1,
+        subscribersCount: 1,
+        isSubscibed: 1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscribedChannel,
+        "subscribed channel is fetched successfully"
+      )
+    );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -11,9 +11,22 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/c/:channelId")
-    .get(getSubscribedChannels)
+    .get(getUserChannelSubscribers) // the users who subscibe to a channel
     .post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+    // the channel which user has subscibed
+router.route("/u/:subscriberId").get(getSubscribedChannels);
 
 export default router
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODA3M2Q2NzQ2MzI0NTRhM2IwN2NhNTgiLCJ1c2VybmFtZSI6ImJlcnNlcmsiLCJmdWxsbmFtZSI6ImJlcnNlcmsgY2hhbm5lbCIsImVtYWlsIjoiYmVyc2VyazUwMEBnbWFpbC5jb20iLCJpYXQiOjE3NDcwMjM2MDEsImV4cCI6MTc0NzExMDAwMX0.JE62luZ3sU92p6x_o8IxVsGrr5EJGBU8WOmdLaODnh0
+// 68073d674632454a3b07ca58
+// berserk
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODA3M2RjZjQ2MzI0NTRhM2IwN2NhNWMiLCJ1c2VybmFtZSI6IndlYmRldiIsImZ1bGxuYW1lIjoid2ViZGV2IGNoYW5uZWwiLCJlbWFpbCI6IndlYmRldjUwMEBnbWFpbC5jb20iLCJpYXQiOjE3NDcwMjM4MTQsImV4cCI6MTc0NzExMDIxNH0.koqs5b0TJl5ifrUjto2z_K2L2KXe2PKuGu91OSxQqOA
+// 68073dcf4632454a3b07ca5c
+// webdev
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODA3M2Y3MDk3ZTY1ZjM2N2Q3MjNiMmUiLCJ1c2VybmFtZSI6ImNhcnRvb24iLCJmdWxsbmFtZSI6ImNhcnRvb24gY2hhbm5lbCIsImVtYWlsIjoiY2FydG9vbjUwMEBnbWFpbC5jb20iLCJpYXQiOjE3NDcwMjM4NjgsImV4cCI6MTc0NzExMDI2OH0._KNOlx6iJYUcUsKirqv-2xVIHTaxLtRoJSppQn9dnZ8
+// 68073f7097e65f367d723b2e
+// cartooon
