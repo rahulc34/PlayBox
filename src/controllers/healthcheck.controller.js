f654c7b58c7c00b66a512b4a3559724ca45a8faa
Rahul-git376 <chaurasiyarahul500@gmail.com>
2025-05-15 23:46:44 +0530

    complete the healthcheck

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,13 @@
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+  //TODO: build a healthcheck response that simply returns the OK status as json with a
+  return res.status(200).json({
+    status: "OK",
+    timestamp: new Date().toISOString()
+  });
+});
+
+export { healthcheck };
